d869e841521efcfccdf1735d8546e3b59867e1e1
Eric Gotkowski <egotkowski@gmail.com>
2024-10-02 22:38:32 -0500

    adding web server components

--- cmd/cyoaweb/main.go
+++ cmd/cyoaweb/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 
 	"github.com/egotch/cyoa"
@@ -10,7 +12,9 @@ import (
 
 func main() {
 
+  //flags
 	filePtr := flag.String("story", "gopher.json", "path + file name of json file containing story data")
+  portPtr := flag.Int("port", 3000, "the port to start the CYOA web app on")
 
 	flag.Parse()
 
@@ -22,5 +26,8 @@ func main() {
 
 	story, err := cyoa.JsonStory(file)
 
-	fmt.Printf("%+v\n", story)
+  h := cyoa.NewHandler(story)
+  fmt.Printf("Starting the server on port: %d\n", *portPtr)
+  log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *portPtr), h))
+
 }
--- story.go
+++ story.go
@@ -2,7 +2,9 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
+	"net/http"
 )
 
 type Story map[string]Chapter
@@ -16,6 +18,50 @@ type Chapter struct {
 type Option struct {
 	Text string `json:"text"`
 	Arc  string `json:"arc"`
+}
+
+var tpl *template.Template
+
+func init() {
+  tpl = template.Must(template.New("").Parse(defaultHandlerTmplt))
+}
+
+var defaultHandlerTmplt string = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Choose Your Own Adventure</title>
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+  <h1>{{.Title}}</h1> 
+    {{range .Paragraphs}}
+      <p>{{.}}</p>
+    {{end}}
+    <ul>
+      {{range .Options}}
+      <li><a href="/{{.Arc}}">{{.Text}}</a></li>
+      {{end}}
+    </ul>
+  </body>
+</html>
+`
+
+func NewHandler(s Story) http.Handler {
+  return handler{s}
+}
+
+type handler struct {
+  s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  err := tpl.Execute(w, h.s["intro"])
+  if err != nil {
+    panic(err)
+  }
 }
 
 func JsonStory(r io.Reader) (Story, error) {
